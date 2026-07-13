7d78e24d21c4afa2f72ba4d0a104cddddfc8acd5
kouhei-souda <meronsoda2716@gmail.com>
2026-07-13 22:28:10 +0900

    feat: 注文機能のテーブルとモデルを追加

---
+++ app/Models/Order.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    protected $fillable = [
+        'user_id',
+        'total_price',
+    ];
+}
